14a09ec920bec5fe14e81c5e9154dcb6c843e3d9
João Brázio <jbrazio@gmail.com>
2017-11-19 21:40:06 +0000

    Adds support for the Arduino IDE

---
+++ ardufocus.ino
@@ -0,0 +1,41 @@
+/**
+ * Ardufocus - Moonlite compatible focuser
+ * Copyright (C) 2017 João Brázio [joao@brazio.org]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Pst !
+ *
+ * This file is just a placeholder so the Arduino IDE likes us, all
+ * the juicy source code is located under the src/ folder, go have a
+ * look.
+ *
+ * If you have issues running this firmware or you'd like to suggest an
+ * improvement for it, go to our Github project page and submit a new
+ * ticket at <https://github.com/jbrazio/ardufocus/issues>.
+ *
+ * We do enjoy some community love <3 <3 <3, share some with us by
+ * forking from <https://github.com/jbrazio/ardufocus/fork/> and submit
+ * a new Push Request.
+ *
+ * If you are new to the Github community and/or how you can contribute to
+ * any hosted project we do recommend a 5 minute reading of "Understanding
+ * the GitHub Flow" from <https://guides.github.com/introduction/flow/>.
+ *
+ * -- the team
+ *
+ */
